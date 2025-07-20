816b698b08200a5f7a82ea5aee6d3e54e291fa00
Maxoduke <maxoduke@outlook.com>
2025-07-21 00:32:58 +0530

    Update to 1.21.8. Update recipe, textures, and README

--- src/main/java/dev/maxoduke/mods/portallinkingcompass/mixins/RangeSelectItemModelPropertiesMixin.java
+++ src/main/java/dev/maxoduke/mods/portallinkingcompass/mixins/RangeSelectItemModelPropertiesMixin.java
@@ -5,6 +5,7 @@ import dev.maxoduke.mods.portallinkingcompass.client.PortalLinkingCompassAngle;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.item.properties.numeric.RangeSelectItemModelProperties;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +18,6 @@ public class RangeSelectItemModelPropertiesMixin
     @Inject(method = "bootstrap", at = @At("TAIL"))
     private static void bootstrap(CallbackInfo ci)
     {
-        RangeSelectItemModelProperties.ID_MAPPER.put(PortalLinkingCompass.ITEM_RESOURCE, PortalLinkingCompassAngle.MAP_CODEC);
+        RangeSelectItemModelProperties.ID_MAPPER.put(ResourceLocation.fromNamespaceAndPath(PortalLinkingCompass.MOD_ID, "angle"), PortalLinkingCompassAngle.MAP_CODEC);
     }
 }
